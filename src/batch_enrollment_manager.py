22354006141845cb05211f5e5abcec8012ed8934
czkay <chong.zi.kang@gmail.com>
2020-06-04 17:13:04 +0800

    Add documentation

--- src/batch_enrollment_manager.py
+++ src/batch_enrollment_manager.py
@@ -1,3 +1,18 @@
+"""A simple desktop-based batch enrollment manager script.
+
+This script reads unenrolled employees' logs in enrollment.txt and shows the logs' details
+one-by-one in a GUI.
+
+The admin can manually input the NRIC corresponding to the employee shown in the GUI.
+
+Upon submission, the employee's NRIC and smartcard ID will be written into persons.txt, hence completing enrollment.
+
+To use, change config.py to paths of your choice. Enter `python batch_enrollment_manager.py`
+to run the script. Remember to install PySimpleGUI (pip install pysimplegui) as this script is dependent on that library.
+
+Logger output can be found in logs.txt.
+"""
+
 import config
 import csv
 import itertools
@@ -7,17 +22,26 @@ import os
 import shutil
 
 def initialise_logger():
+    """Initialises a logger to log actions performed, as well as warnings/potential issues."""
+
     logging.basicConfig(filename='logs.txt', filemode='w', 
             format='%(asctime)s %(levelname)s %(message)s',
             datefmt='%d/%m/%Y %I:%M:%S %p', level=logging.INFO)
 
 def join_paths():
+    """Joins the base paths in config.py with the files that this script needs to run."""
+
     unenrolled_details_path = os.path.join(config.BASE_UNENROLLED_DETAILS_PATH, 'enrollment.txt')
     photos_path = config.BASE_PHOTOS_PATH
     full_details_path = os.path.join(config.BASE_FULL_DETAILS_PATH, 'persons.txt')
     return (unenrolled_details_path, photos_path, full_details_path)
 
 def get_unenrolled_data(details_path, base_photos_path):
+    """Reads enrollment.txt to obtain the logs of unenrolled employees.
+    
+    If an unenrolled employee has been logged multiple times (as seen from multiple logs with the same
+    smartcard ID), only the latest log will be taken.
+    """
     with open(details_path, "r") as f:
         data = []
         reader = csv.DictReader(f)
@@ -32,12 +56,15 @@ def get_unenrolled_data(details_path, base_photos_path):
     return data # return as a list of lists, with each sublist containing a log of an unenrolled person
 
 def clear_unenrolled_data(details_path):
+    """After batch enrollment has ended, clear the logs from enrollment.txt"""
+
     with open(details_path, "w") as f:
         f.write('photo_path,timestamp,id\n')
     logging.info("Cleared enrollment.txt.")
 
 def initialise_window(log):
-    print(log)
+    """Initialises the window that will be displayed to the admin."""
+
     sg.theme('Dark')  
     layout = [  [sg.Image(log[0], key='PHOTO')],
         [sg.Text('Timestamp ' + log[1], key='TIMESTAMP')],
@@ -51,6 +78,8 @@ def initialise_window(log):
 
 # loads the window to UI and waits for/processes input
 def load_window(window, log, full_details_path):
+    """Loads the window to the GUI, and processes value input and keypresses."""
+
     id = log[2]
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Discard':
@@ -62,6 +91,13 @@ def load_window(window, log, full_details_path):
     window.close()
 
 def update_full_details(nric, id, path):
+    """Update persons.txt with the NRIC and smartcard ID of the employee, hence completing enrollment."""
+    
+    with open(path, 'a', newline='') as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow([nric, id])
+        logging.info("Success!")
+
     # with open(path, 'r') as f:
     #     reader = csv.DictReader(f)
     #     data = [row for row in reader]
@@ -77,14 +113,10 @@ def update_full_details(nric, id, path):
     #             writer.writerow([nric, id])
     #         else:
     #             writer.writerow([row['nric'], row['id']])
-    
-    # append nric and id to persons.txt
-    with open(path, 'a', newline='') as f:
-        writer = csv.writer(f, delimiter=',')
-        writer.writerow([nric, id])
-        logging.info("Success!")
 
 def enroll(data_list, full_details_path):
+    """Performs batch enrollment for all unenrolled employees found in enrollment.txt."""
+
     if data_list: # if there are unenrolled persons
         for log in data_list:
             window = initialise_window(log)
@@ -94,6 +126,8 @@ def enroll(data_list, full_details_path):
         logging.info("No unenrolled persons found.")
 
 def main():
+    """Main driver function."""
+
     initialise_logger()
 
     logging.info("Program running...")
